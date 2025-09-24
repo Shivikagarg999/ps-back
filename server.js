7578f2491c29a77bdde7e8f766c4c86f2d01ec46
shivikagarg999 <shivikagarg91@gmail.com>
2025-09-24 13:13:26 +0530

    url

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", "https://ps-admin-alpha.vercel.app/"]
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
