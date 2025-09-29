8e1389be3373e19b3d87e79814f3590dbbf8538a
shivikagarg999 <shivikagarg91@gmail.com>
2025-09-29 16:23:36 +0530

    email unique false

--- models/user/user.js
+++ models/user/user.js
@@ -9,7 +9,6 @@ const userSchema = new mongoose.Schema({
   },
   email: {
     type: String,
-    unique: true,
     lowercase: true,
     match: [/^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/, 'Please enter a valid email']
   },
